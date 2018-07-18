d1b0571b60c9460568f3e1c4c121085e352ef7b5
none <none>
2018-07-18 11:08:30 -0400

    Commiting ipython notebook for the data download.

--- download_database.py
+++ download_database.py
@@ -1,11 +1,12 @@
 import os
 import time
 import requests
-
 import pycurl
+
 
 def create_data_structure():
     """
+    ensure a downloads folder exist and returns the list of files in it
     """
     if 'downloads' not in os.listdir('.'):
         os.mkdir('downloads')
@@ -17,22 +18,22 @@ def create_data_structure():
 
 def clear_data_structure():
     """
+    erase the downloads folder to start fresh
     """
-    if 'downloads' not in os.listdir('.'):
+    if 'downloads' in os.listdir('.'):
         os.system('rm -r downloads')
     return
 
 
 def guess_names(file_name):
     """
+    return a list of potential file names to download
     """
     name = file_name[:-9]
-    #print name
 
     possible_names = []
-    #for v in range(9, 0, -1):
+    # for v in range(9, 0, -1):
     for v in range(6, 0, -1):
-    #for v in range(1, 0, -1):
         for ext in ['', '_0', '_1']:
             for pre in ['%20', '']:
                 for rank in ['Rankings', 'Ranking']:
@@ -44,8 +45,11 @@ def guess_names(file_name):
     return possible_names
 
 
-def download_file(base_url, file_name, save_file_name, file_size_lowerlim=0):
+def download_file(base_url, file_name, save_file_name, file_size_lowerlim=0,
+                  sleep_time=3):
     """
+    download a file, save it as save_file_name, erase it if it is smaller than
+    file_size_lower_limit (in Bytes)
     """
     url = os.path.join(base_url, file_name)
     print(file_name)
@@ -62,7 +66,7 @@ def download_file(base_url, file_name, save_file_name, file_size_lowerlim=0):
     curl.setopt(pycurl.WRITEDATA, file_)
     curl.perform()
     file_.close()
-    time.sleep(3)
+    time.sleep(sleep_time)
 
     file_size = os.path.getsize(os.path.join('downloads', save_file_name))
     if file_size < file_size_lowerlim:
@@ -77,6 +81,7 @@ def download_file(base_url, file_name, save_file_name, file_size_lowerlim=0):
         return True
 
 
+sleep_time = 3
 downloaded_files = create_data_structure()
 
 base_url = os.path.join("http://www.countyhealthrankings.org", "sites",
@@ -92,26 +97,16 @@ states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
           'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
           'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
           'West Virginia', 'Wisconsin', 'Wyoming']
-#states = states[0:1]
-
-"""
-add v1_0 for Michigan and Missouri...
-"""
-
 
 for year in [2005, 2014, 2015, 2016]:
     base_url = 'https://www.cdc.gov/nchs/pressroom/sosmap/suicide-mortality/'
     url_file_name = 'SUICIDE{0:d}.csv'.format(year)
     success = download_file(base_url, url_file_name, url_file_name,
-                            file_size_lowerlim=1000)
+                            file_size_lowerlim=1000, sleep_time=sleep_time)
     if not success:
         exit("Error downloading suicide data")
 
-#years = range(2011, 2012)
-#years = range(2018, 2019)
-#years = range(2011, 2019)
-years = range(2018, 2009, -1)
-
+years = range(2010, 2019)
 for state in states:
     for year in years:
         print state, year
@@ -125,8 +120,9 @@ for state in states:
             print '---'
             possible_url_file_names = guess_names(file_name)
             for url_file_name in possible_url_file_names:
-                success = download_file(base_url, url_file_name, save_file_name,
-                                        file_size_lowerlim=29000)
+                success = download_file(base_url, url_file_name,
+                                        save_file_name,
+                                        file_size_lowerlim=29000,
+                                        sleep_time=sleep_time)
                 if success:
                     break
-
